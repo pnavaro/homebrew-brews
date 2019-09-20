d19e100b9700cfd8f91a10885bee5a299fa638c9
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-20 20:48:37 +0200

    Update selalib.rb

--- selalib.rb
+++ selalib.rb
@@ -1,21 +1,20 @@
 class Selalib < Formula
   desc "Semi-lagrangian library"
   homepage "http://selalib.gforge.inria.fr"
-  url "https://gitlab.mpcdf.mpg.de/clapp/selalib.git"
+  head "https://gitlab.mpcdf.mpg.de/clapp/selalib.git", :branch => "develop"
 
-  keg_only ""
+  keg_only ". Try simulations "
 
   depends_on "cmake" => :build
   depends_on "open-mpi"
   depends_on "fftw"
-  depends_on "pnavaro/brews/hdf5-parallel"
   
   fails_with :clang
   env :std
   
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCMAKE_BUILD_TYPE=Release", "-DHDF5_PARALLEL_ENABLES=ON", "-DHDF5_ROOT=/usr/local/opt/hdf5-parallel"
+      system "cmake", "..", *std_cmake_args, "-DCMAKE_BUILD_TYPE=Release", "-DHDF5_PARALLEL_ENABLED=ON", "-DHDF5_ROOT=/usr/local/opt/hdf5-parallel", "-DCMAKE_INSTALL_PREFIX=/usr/local/opt/selalib"
       system "make", "install"
     end
   end
