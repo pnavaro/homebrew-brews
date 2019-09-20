37cc394ce0dbee38970d295842127d41e44243de
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-20 21:31:46 +0200

    Update selalib.rb

--- selalib.rb
+++ selalib.rb
@@ -3,7 +3,7 @@ class Selalib < Formula
   homepage "http://selalib.gforge.inria.fr"
   head "https://gitlab.mpcdf.mpg.de/clapp/selalib.git", :branch => "develop"
 
-  keg_only "\n - Copy the simulation directory: \n $ cp -r /usr/local/opt/selalib/bin/selalib_simulations/parallel/bsl_vp_1d1v_cart . \n $ cd bsl_vp_1d1v_cart \n - Run simulation \n $ mpirun -np 4 ./sim_bsl_vp_1d1v_cart vpsim2d_cartesian_input \n - Run included gnuplot script \n $ gnuplot --persist vpsim2d_cartesian.gnu "
+  keg_only "it is not a public software. \n - Copy the simulation directory: \n $ cp -r /usr/local/opt/selalib/bin/selalib_simulations/parallel/bsl_vp_1d1v_cart . \n $ cd bsl_vp_1d1v_cart \n - Run simulation \n $ mpirun -np 4 ./sim_bsl_vp_1d1v_cart vpsim2d_cartesian_input \n - Run included gnuplot script \n $ gnuplot --persist vpsim2d_cartesian.gnu "
 
   depends_on "cmake" => :build
   depends_on "open-mpi"
